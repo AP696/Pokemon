e25ba8343ac93ee3d1ef46e366e0c924ca12357f
AP696 <addyphilips66@gmail.com>
2023-10-10 11:31:21 +0100

    cards rendered

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    response = requests.get('https://pokeapi.co/api/v2/pokemon?limit=100')
+    data = response.json()
+    pokemon_list = data['results']
+    return render_template('index.html', pokemon_list=pokemon_list)
+
+if __name__ == '__main__':
+    app.run(debug=True)
